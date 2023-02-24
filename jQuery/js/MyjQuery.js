e2d5dc940dafd812c33aa63beb595ac3d9991901
Tarun <73988795+tarun9072@users.noreply.github.com>
2023-02-24 21:32:25 +0530

    My today Learning

--- jQuery/js/MyjQuery.js
+++ jQuery/js/MyjQuery.js
@@ -4,14 +4,39 @@ $(document).ready(function () {
     //     console.log('You clicked on Paragraph')
     //     $(this).hide();
     // });
-    $('p').on(
-        {
-            click: function () {
-                console.log("Thanks for Clicking");
+    // $('p').on(
+    //     {
+    //         click: function () {
+    //             console.log("Thanks for Clicking");
 
-            },
-            mouseleave: function () {
-                console.log("Mouse Leave")
-            }
-        })
+    //         },
+    //         mouseleave: function () {
+    //             console.log("Mouse Leave")
+    //         }
+    //     })
+    //Creating Animation
+    // $('#wiki').slideUp(5000);
+    // $('#wiki').slideDown(3000, function () {
+    //     console.log("Animation Completed");
+    // })
+    //---------------------------------------------
+    // $("div").hover(function (){
+    //     $("div").fadeToggle(1000, function(){
+    //         console.log("Toggle Done");
+    //     })
+    // })
+    //Events
+    // $("#btn").click(function () {
+    //     $("#img1").css('width', '500px')
+    // })
+    // $("#btn").dblclick(function () {
+    //     $("#img1").css('width', '300px')
+    // })
+    $("img").mouseenter(function(){
+        $("img").css('width', '500px')
+    })
+    $("img").mouseleave(function(){
+        $("img").css('width', '300px')
+    })
+
 })
