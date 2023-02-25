bfbd168c125cd0389c01675870c6285b12bf6c4e
Tarun <73988795+tarun9072@users.noreply.github.com>
2023-02-25 18:55:42 +0530

    My Todays Learning

--- jQuery/js/MyjQuery.js
+++ jQuery/js/MyjQuery.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    console.log("We are Using the Jquery");
+    // console.log("We are Using the Jquery");
     // $('p').click(function () {
     //     console.log('You clicked on Paragraph')
     //     $(this).hide();
@@ -23,7 +23,8 @@ $(document).ready(function () {
     // $("div").hover(function (){
     //     $("div").fadeToggle(1000, function(){
     //         console.log("Toggle Done");
-    //     })
+    //     }) 
+    //This is an example of Call Back Function too
     // })
     //Events
     // $("#btn").click(function () {
@@ -32,11 +33,40 @@ $(document).ready(function () {
     // $("#btn").dblclick(function () {
     //     $("#img1").css('width', '300px')
     // })
-    $("img").mouseenter(function(){
-        $("img").css('width', '500px')
+    // $("img").mouseenter(function(){
+    //     $("img").css('width', '500px')
+    // })
+    // $("img").mouseleave(function(){
+    //     $("img").css('width', '300px')
+    // })
+
+    //DOM Manipulation
+
+    // $("#div1").click(function(){
+    //     var x = $("#p1").html();
+    //     $("#span1").text(x)
+    // })
+    // $("#p1").click(function(){
+    // alert($("#p1").html())   //It shows Html if written
+    //    alert($("#p1").text())      // It only Shows the Text
+    // })
+
+    // $("#btn1").click(function(){
+    //     $(".para").attr("id", "p2")     //If you change any attribute then dont access them by that attribute that will going to be changed. It will not throw an error but it does not work
+    // })
+    // $("#p1").click(function(){
+    //     alert($(".para").attr("id"))
+    // })
+    //Append or Prepend 
+    $("#btn1").click(function(){
+        $("#div1").append('<p>This is a para append by jQuery</p>')
+        
     })
-    $("img").mouseleave(function(){
-        $("img").css('width', '300px')
+    $("#btn2").click(function(){
+        $("#div1").prepend('<p>This is a para Prepend by jQuery</p>')
     })
+    // Dragable intraction
+    // $(".box").draggable();
+
 
 })
