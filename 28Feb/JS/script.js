f246cd2c5da42da6ab0e9af315d60c51315e3923
Tarun <73988795+tarun9072@users.noreply.github.com>
2023-03-01 14:34:27 +0530

    Eddited code

--- 28Feb/JS/script.js
+++ 28Feb/JS/script.js
@@ -1,5 +1,31 @@
-var emptyRow="<tr><td colspan='5'> No Record Available</td></tr>";
-$(document).ready(function(){
+var emptyRow = "<tr><td colspan='5'> No Record Available</td></tr>";
+$(document).ready(function () {
+    $("#table tbody").append(emptyRow);
+
+    $("#btnAdd").click(function () {
+        var name = $("#name").val().trim();
+        var address = $("#address").val().trim();
+        var country = $("#country").val().trim();
+        var MobileNumber = $("#mNumber").val().trim();
+        if (name != "" && address != "" && country != "" && MobileNumber != "") {
+            if ($("#table tbody").children().children().length == 1) {
+                $("#table tbody").html("");
+            }
+
+            var dynamicTr = "<tr><td>" + name + "</td><td>" + address + "</td><td>" + country + "</td><td>" + MobileNumber + "</td></tr>";
+            $("#table tbody").append(dynamicTr);
+            $("#name").val("");
+            $("#address").val("");
+            $("#country").val("");
+            $("#mNumber").val("");
+            
+            
+        }
+        else {
+            alert("Please Provide Values")
+        }
+
+    });
 
 
 
