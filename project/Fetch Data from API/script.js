4a799094ecfe410fd05213bfe519f321763b5308
Tarun <73988795+tarun9072@users.noreply.github.com>
2023-03-03 18:32:18 +0530

    Fetch Data from Api In Table

---
+++ project/Fetch Data from API/script.js
@@ -0,0 +1,21 @@
+// fetch("https://fakestoreapi.com/products").then((data) => {
+//     return data.json(); // It converts the Data
+// })
+fetch("https://fakestoreapi.com/products")
+.then(data => data.json())
+.then((objectdata) => {
+    console.log(objectdata);
+    let tabledata = "";
+    objectdata.map((values) => {
+        tabledata += `
+        <tr> 
+            <td>${values.title}</td>
+            <td>${values.description}</td>
+            <td>${values.price}</td>
+            <td><img src="${values.image}"></img></td>
+            <td>${values.category}</td>
+            </tr>
+        `;
+    });
+    document.getElementById("tablebody").innerHTML=tabledata;
+})
