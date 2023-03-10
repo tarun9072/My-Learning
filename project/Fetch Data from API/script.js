31485eabf6db56c23f87ee7d660ee43976ca7ab2
Tarun <73988795+tarun9072@users.noreply.github.com>
2023-03-10 13:19:09 +0530

    some changes

--- project/Fetch Data from API/script.js
+++ project/Fetch Data from API/script.js
@@ -1,7 +1,10 @@
 // fetch("https://fakestoreapi.com/products").then((data) => {
 //     return data.json(); // It converts the Data
 // })
-fetch("https://fakestoreapi.com/products")
+
+
+
+fetch("https://jsonplaceholder.typicode.com/photos")
 .then(data => data.json())
 .then((objectdata) => {
     console.log(objectdata);
@@ -10,11 +13,11 @@ fetch("https://fakestoreapi.com/products")
     objectdata.map((values) => {
         tabledata += `
         <tr> 
+            <td>${values.id}</td>
             <td>${values.title}</td>
-            <td>${values.description}</td>
-            <td>${values.price}</td>
-            <td><img src="${values.image}"></img></td>
-            <td>${values.title}</td>
+        
+            <td><img src="${values.url}"></img></td>
+            <td><img src="${values.thumbnailUrl}"></img</td>
             </tr>
         `;
     });
